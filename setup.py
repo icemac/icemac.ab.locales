92b9093edd25ace7958dc0fba7c57370a56da672
Michael Howitz <mh@gocept.com>
2011-10-28 21:04:09 +0000

    Preparing release 0.8

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ import setuptools
 def read(*path_elements):
     return "\n\n" + file(os.path.join(*path_elements)).read()
 
-version = '0.8dev'
+version = '0.8'
 
 setuptools.setup(
     name='icemac.ab.locales',
