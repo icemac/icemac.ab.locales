ac3ba0e8a0955cc0e5dee8370bce6cc164692a90
Michael Howitz <mh@gocept.com>
2016-02-13 21:33:11 +0100

    PEP-8

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2009-2014 Michael Howitz
-# See also LICENSE.txt
-
 import os.path
 import setuptools
 
+
 def read(*path_elements):
+    """Read a file."""
     return file(os.path.join(*path_elements)).read()
 
 version = '2.8.dev0'
@@ -38,28 +37,28 @@ setuptools.setup(
         'Programming Language :: Python :: Implementation',
         'Programming Language :: Python :: Implementation :: CPython',
         'Topic :: Internet :: WWW/HTTP',
-        ],
+    ],
     packages=setuptools.find_packages('src'),
-    package_dir = {'': 'src'},
-    namespace_packages = ['icemac', 'icemac.ab'],
+    package_dir={'': 'src'},
+    namespace_packages=['icemac', 'icemac.ab'],
     include_package_data=True,
     zip_safe=False,
     install_requires=[
         'setuptools',
         'zope.i18n',
-        ],
-    extras_require = dict(
-        extract = [
+    ],
+    extras_require=dict(
+        extract=[
             'icemac.ab.calendar',
             'icemac.ab.importer',
             'icemac.ab.importxls',
             'icemac.addressbook',
-            ],
-        extractZopeMimetype = [
+        ],
+        extractZopeMimetype=[
             'zope.mimetype',
-            ],
+        ],
         compile=[
             'python-gettext'
-            ],
-        ),
-    )
+        ],
+    ),
+)
