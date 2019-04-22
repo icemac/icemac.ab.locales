5ff2f4674abc0ff7422bdab144fd28d228a4b781
Michael Howitz <mh@gocept.com>
2019-04-22 09:57:34 +0200

    Back to development: 2.22

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(*path_elements):
     with open(os.path.join(*path_elements)) as f:
         return f.read()
 
-version = '2.21'
+version = '2.22.dev0'
 
 setuptools.setup(
     name='icemac.ab.locales',
